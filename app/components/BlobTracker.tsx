82db4cf175142f20778f1b2c7d7747613a3d042f
Dachan <90619921+chani017@users.noreply.github.com>
2026-02-03 02:54:44 +0900

    floating

--- app/components/BlobTracker.tsx
+++ app/components/BlobTracker.tsx
@@ -404,7 +404,7 @@ export default function BlobTracker() {
                 ? `fixed z-50 shadow-[0_20px_50px_rgba(0,0,0,0.5)] border border-white transform pointer-events-none ${
                     aspectRatio && aspectRatio > 1 
                       ? 'top-2 left-[4%] w-[92%]' 
-                      : 'top-2 left-4 w-[45%]'
+                      : 'top-0 left-4 w-[45%]'
                   }` 
                 : 'relative w-full h-full'
             }`}>
